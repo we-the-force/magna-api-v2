7384882f22ead022208fca13b3eb35ff4621925d
Will Alvarez <will.alvarez@gmail.com>
2021-01-06 17:25:43 -0600

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'fbe62a12f759b8598ace949c01a234fa'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9985a246-9abc-40fd-9918-cc9672665bb7'
+};
