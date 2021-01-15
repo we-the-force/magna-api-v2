575988bf6a008935122bd41927f62b51c220ccb0
Will Alvarez <will.alvarez@gmail.com>
2021-01-15 12:49:13 -0600

    plugin import-content

--- config/database.js
+++ config/database.js
@@ -1,21 +1,3 @@
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'mysql',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'magnamysql'),
-        username: env('DATABASE_USERNAME', 'root'),
-        password: env('DATABASE_PASSWORD', 'P4r4n64r1'),
-        ssl: env.bool('DATABASE_SSL', false),
-      },
-      options: {}
-    },
-  },
-});
 // module.exports = ({ env }) => ({
 //   defaultConnection: 'default',
 //   connections: {
@@ -24,13 +6,31 @@ module.exports = ({ env }) => ({
 //       settings: {
 //         client: 'mysql',
 //         host: env('DATABASE_HOST', '127.0.0.1'),
-//         port: env.int('DATABASE_PORT', 8889),
-//         database: env('DATABASE_NAME', 'magnav2'),
+//         port: env.int('DATABASE_PORT', 3306),
+//         database: env('DATABASE_NAME', 'magnamysql'),
 //         username: env('DATABASE_USERNAME', 'root'),
-//         password: env('DATABASE_PASSWORD', 'root'),
+//         password: env('DATABASE_PASSWORD', 'P4r4n64r1'),
 //         ssl: env.bool('DATABASE_SSL', false),
 //       },
 //       options: {}
 //     },
 //   },
 // });
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'mysql',
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env.int('DATABASE_PORT', 8889),
+        database: env('DATABASE_NAME', 'magnav2'),
+        username: env('DATABASE_USERNAME', 'root'),
+        password: env('DATABASE_PASSWORD', 'root'),
+        ssl: env.bool('DATABASE_SSL', false),
+      },
+      options: {}
+    },
+  },
+});
