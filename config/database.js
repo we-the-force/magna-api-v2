95ab624af6bf2d1708e52c1792ea9eab3051663b
Will Alvarez <will.alvarez@gmail.com>
2021-01-18 15:00:48 -0600

    mongo db

--- config/database.js
+++ config/database.js
@@ -1,21 +1,21 @@
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'mysql',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'magnamysql'),
-        username: env('DATABASE_USERNAME', 'root'),
-        password: env('DATABASE_PASSWORD', 'P4r4n64r1'),
-        ssl: env.bool('DATABASE_SSL', false),
-      },
-      options: {}
-    },
-  },
-});
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'bookshelf',
+//       settings: {
+//         client: 'mysql',
+//         host: env('DATABASE_HOST', '127.0.0.1'),
+//         port: env.int('DATABASE_PORT', 3306),
+//         database: env('DATABASE_NAME', 'magnamysql'),
+//         username: env('DATABASE_USERNAME', 'root'),
+//         password: env('DATABASE_PASSWORD', 'P4r4n64r1'),
+//         ssl: env.bool('DATABASE_SSL', false),
+//       },
+//       options: {}
+//     },
+//   },
+// });
 // module.exports = ({ env }) => ({
 //   defaultConnection: 'default',
 //   connections: {
@@ -34,3 +34,23 @@ module.exports = ({ env }) => ({
 //     },
 //   },
 // });
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'mongoose',
+      settings: {
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        srv: env.bool('DATABASE_SRV', false),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'supervizor-v2'),
+        username: env('DATABASE_USERNAME', null),
+        password: env('DATABASE_PASSWORD', null),
+      },
+      options: {
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        ssl: env.bool('DATABASE_SSL', false),
+      },
+    },
+  },
+});
