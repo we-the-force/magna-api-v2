bdee6c3d91d62db2bf0aafcfd31806f626ede6cf
Will Alvarez <will.alvarez@gmail.com>
2021-01-28 18:28:34 -0600

    logo

--- config/database.js
+++ config/database.js
@@ -16,41 +16,41 @@
 //     },
 //   },
 // });
-// module.exports = ({ env }) => ({
-//   defaultConnection: 'default',
-//   connections: {
-//     default: {
-//       connector: 'bookshelf',
-//       settings: {
-//         client: 'mysql',
-//         host: env('DATABASE_HOST', '127.0.0.1'),
-//         port: env.int('DATABASE_PORT', 8889),
-//         database: env('DATABASE_NAME', 'magnav2'),
-//         username: env('DATABASE_USERNAME', 'root'),
-//         password: env('DATABASE_PASSWORD', 'root'),
-//         ssl: env.bool('DATABASE_SSL', false),
-//       },
-//       options: {}
-//     },
-//   },
-// });
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
-      connector: 'mongoose',
+      connector: 'bookshelf',
       settings: {
+        client: 'mysql',
         host: env('DATABASE_HOST', '127.0.0.1'),
-        srv: env.bool('DATABASE_SRV', false),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'supervizor-v2'),
-        username: env('DATABASE_USERNAME', null),
-        password: env('DATABASE_PASSWORD', null),
-      },
-      options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        port: env.int('DATABASE_PORT', 8889),
+        database: env('DATABASE_NAME', 'magnav2'),
+        username: env('DATABASE_USERNAME', 'root'),
+        password: env('DATABASE_PASSWORD', 'root'),
         ssl: env.bool('DATABASE_SSL', false),
       },
+      options: {}
     },
   },
 });
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'mongoose',
+//       settings: {
+//         host: env('DATABASE_HOST', '127.0.0.1'),
+//         srv: env.bool('DATABASE_SRV', false),
+//         port: env.int('DATABASE_PORT', 27017),
+//         database: env('DATABASE_NAME', 'supervizor-v2'),
+//         username: env('DATABASE_USERNAME', null),
+//         password: env('DATABASE_PASSWORD', null),
+//       },
+//       options: {
+//         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+//         ssl: env.bool('DATABASE_SSL', false),
+//       },
+//     },
+//   },
+// });
