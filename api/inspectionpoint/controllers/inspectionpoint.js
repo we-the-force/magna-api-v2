94e814e2ea70d35d7e0d0909552edfb4dd39b6ad
Will Alvarez <will.alvarez@gmail.com>
2021-08-16 11:58:13 -0500

    inspection point limitless

--- api/inspectionpoint/controllers/inspectionpoint.js
+++ api/inspectionpoint/controllers/inspectionpoint.js
@@ -1,8 +1,27 @@
 'use strict';
-
+const { sanitizeEntity } = require('strapi-utils');
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+
+  async find(ctx) {
+    let entities;
+    ctx.query = {
+    ...ctx.query,
+    _limit: -1,
+    };
+    if (ctx.query._q) {
+    entities = await strapi.services.inspectionpoint.search(ctx.query);
+    } else {
+    entities = await strapi.services.inspectionpoint.find(ctx.query);
+    }
+
+    return entities.map((entity) =>
+    sanitizeEntity(entity, { model: strapi.models.inspectionpoint })
+    );
+  },
+
+};
