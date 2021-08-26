633844c973fa217ae8bcff31ff416028cab9ee0f
Will Alvarez <will.alvarez@gmail.com>
2021-08-26 18:42:22 -0500

    alert to resp

--- api/report/controllers/report.js
+++ api/report/controllers/report.js
@@ -42,7 +42,7 @@ module.exports =  {
             result(defect.alert_level).then(async function(resp) {
               console.log('resp query');
               console.log(resp);
-              const alert = resp.toJSON();
+              const alert = resp;
               if (alert.supervisors.length > 0) {
                 for (let supervisor of alert.supervisors) {
                   // console.info(supervisor);
